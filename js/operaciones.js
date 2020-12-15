ccf6ed19f9bab30b122bf05588afd2037f050c74
Oscar Arenas <oharenas@gmail.com>
2020-12-15 08:34:18 -0500

    Código javascript para validación de datos.

--- js/operaciones.js
+++ js/operaciones.js
@@ -2,14 +2,11 @@ function validarUsuario() {
     var username = document.getElementById("usuario").value;
     var contrasenia = document.getElementById("contrasenia").value;
 
-
-
     if (contrasenia.length > 7) {
-        alert("Iniciar sesión: " + username + ", " + contrasenia)
-    }
-    else
+        //alert("Iniciar sesión: " + username + ", " + contrasenia);
+        window.location.href = "html/gestion_imagenes.html";
+    } else
         alert("La contraseña debe tener más de 7 carácteres.")
-
 }
 
 function registrarUsuario() {
@@ -19,17 +16,81 @@ function registrarUsuario() {
 
     if (username.length > 7) {
         if (contrasenia.length > 7) {
-            let patron = /[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+/;
+            var patron = /[A-Za-z0-9]+@([A-Za-z0-9]+\.)+[A-Za-z0-9]+/;
+
             if (correo.match(patron)) {
                 alert("Registrar Usuario: " + username + ", " + contrasenia + ", " + correo)
             } else
                 alert("Correo no válido")
-        }
-        else
+        } else
             alert("La contraseña debe tener más de 7 carácteres.")
+    } else {
+        alert("El usuario debe tener más de 7 carácteres.");
+        document.getElementById("usuario").focus()
+    }
+}
+
+function recuperarContrasenia() {
+    var correo = document.getElementById("email").value;
+
+    var patron = /[A-Za-z0-9]+@([A-Za-z0-9]+\.)+[A-Za-z0-9]+/;
+
+    if (correo.match(patron)) {
+        alert("Enlace enviado al correo " + correo + " para recuperar contraseña.")
     } else
-        alert("El usuario debe tener más de 7 carácteres.")
+        alert("Correo no válido")
 }
 
 function subirImagen() {
+    var nombre = document.getElementById("nombre").value;
+    var descripcion = document.getElementById("descripcion").value;
+
+    if (nombre.length > 0) {
+        if (descripcion.length > 0) {
+        } else {
+            alert("Ingrese la descripción de la imagen.")
+        }
+    } else {
+        alert("Ingrese el nombre de la imagen.")
+    }
+}
+
+function buscarImagen() {
+    var dato = document.getElementById("datoBusqueda").value;
+
+    if (dato.length > 0) {
+
+    } else {
+        alert("Ingrese el dato de búsqueda")
+    }
+}
+
+function validarDatoActualizar() {
+    var dato = document.getElementById("datoActualizar").value;
+
+    if (dato.length > 0) {
+
+    } else {
+        alert("Ingrese la palabra clave de búsqueda.")
+    }
+}
+
+function validarDatoDescargar() {
+    var dato = document.getElementById("datoDescargar").value;
+
+    if (dato.length > 0) {
+
+    } else {
+        alert("Ingrese la palabra clave de búsqueda.")
+    }
+}
+
+function validarDatoEliminar() {
+    var dato = document.getElementById("eliminar").value;
+
+    if (dato.length > 0) {
+
+    } else {
+        alert("Ingrese la palabra clave de búsqueda.")
+    }
 }
